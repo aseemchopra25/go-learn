fefe3e514db57b5700fa15b33cc3fa092d44139a
aseemchopra25 <aseemchopra@protonmail.com>
2022-06-12 04:47:18 +1000

    :sparkles: go json decoding

--- 22json/main.go
+++ 22json/main.go
@@ -17,7 +17,8 @@ type pii struct {
 func main() {
 	fmt.Println("Welcome to JSON in golang")
 	// Converting data into json
-	EncodeJson()
+	// EncodeJson()
+	DecodeJson()
 }
 
 func EncodeJson() {
@@ -36,5 +37,41 @@ func EncodeJson() {
 	}
 
 	fmt.Printf("%s\n", finalJson)
+
+}
+
+func DecodeJson() {
+	jsonDataFromWeb := []byte(`
+	{
+		"my cool name": "Aseem",
+		"Age": 5,
+		"Address": "Australia",
+		"tags": ["pentesting","golang"]
+	}
+	`)
+
+	var mypii pii
+
+	checkValid := json.Valid(jsonDataFromWeb)
+
+	if checkValid {
+		fmt.Println("JSON was valid")
+		json.Unmarshal(jsonDataFromWeb, &mypii)
+		fmt.Printf("%#v\n", mypii)
+
+	} else {
+		fmt.Println("JSON wasn't valid")
+	}
+
+	// cases where data needs to be added to key value pairs
+
+	var myonlinedata map[string]interface{} //don't know what kind of data is coming up use interface as anything could come up
+
+	json.Unmarshal(jsonDataFromWeb, &myonlinedata)
+	fmt.Printf("%#v\n", myonlinedata)
+
+	for key, value := range myonlinedata {
+		fmt.Printf("Key is %v and value is %v and the type is %T\n", key, value, value)
+	}
 
 }

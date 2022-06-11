a4504bad71010362662e0708dd4dfe635b2e69ad
aseemchopra25 <aseemchopra@protonmail.com>
2022-06-12 04:12:33 +1000

    :sparkles: go get post post form

--- 21webverb/main.go
+++ 21webverb/main.go
@@ -4,35 +4,37 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
 func main() {
 	fmt.Println("Web Verb Video")
 	// PerformGetRequest()
-	PerformPostJsonRequest()
+	// PerformPostJsonRequest()
+	PerformPostFormRequest()
 }
 
 func PerformGetRequest() {
-	const myurl = "https://google.com"
+	const myurl = "http://localhost:8000"
 	response, err := http.Get(myurl)
 	if err != nil {
 		panic(err)
 	}
 	defer response.Body.Close()
-	fmt.Println("Status code: ", response.StatusCode)
-	fmt.Println("Content Length is: ", response.ContentLength)
-	content, _ := ioutil.ReadAll(response.Body)
+	// fmt.Println("Status code: ", response.StatusCode)
+	// fmt.Println("Content Length is: ", response.ContentLength)
+	// content, _ := ioutil.ReadAll(response.Body)
 	// fmt.Println(string(content))
-	var responseString strings.Builder
-	byteCount, _ := responseString.Write(content)
-	fmt.Println(responseString.String())
-	fmt.Println(byteCount)
+	// var responseString strings.Builder
+	// byteCount, _ := responseString.Write(content)
+	// fmt.Println(responseString.String())
+	// fmt.Println(byteCount)
 }
 
 func PerformPostJsonRequest() {
 	// Change this to a server that actually processes/sends json
-	const myurl = "https://www.google.com"
+	const myurl = "http://localhost:8000/post"
 
 	// fake JSON payload
 	requestBody := strings.NewReader(`
@@ -49,6 +51,27 @@ func PerformPostJsonRequest() {
 	defer response.Body.Close()
 
 	content, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(content))
+
+}
+
+func PerformPostFormRequest() {
+	const myurl = "http://localhost:8000/postform"
+
+	//formdate
+	data := url.Values{}
+	data.Add("firstname", "aseem")
+	data.Add("lastname", "chopra")
+	data.Add("email", "aseemch")
+
+	response, err := http.PostForm(myurl, data)
+
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+	content, _ := ioutil.ReadAll(response.Body)
+
 	fmt.Println(string(content))
 
 }
